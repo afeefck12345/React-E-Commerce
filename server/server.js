926c34324b53e6010cf9869f49eeedd63dd5b4f6
Afeef Ck <afeefck7@gmail.com>
2026-05-18 13:19:07 +0530

    fixed cors and cleaned server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,6 @@
 import "./config/env.js";
 import app from "./app.js";
 import connectDB from "./config/db.js";
-import cors from "cors"
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://react-e-commerce-omega-three.vercel.app",
-    "https://react-e-commerce-git-main-afeef-ck-s-projects.vercel.app"
-  ],
-  credentials: true
-}));
 
 const PORT = process.env.PORT || 5000;
 
